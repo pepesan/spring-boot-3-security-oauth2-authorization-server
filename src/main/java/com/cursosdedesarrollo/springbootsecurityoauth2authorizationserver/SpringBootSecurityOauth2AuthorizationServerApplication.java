557e78786a73e10136f3914ccb507e66dbe6e8c9
David Vaquero <pepesan@gmail.com>
2025-06-25 19:41:59 +0200

    commit inicial

--- src/main/java/com/cursosdedesarrollo/springbootsecurityoauth2authorizationserver/SpringBootSecurityOauth2AuthorizationServerApplication.java
+++ src/main/java/com/cursosdedesarrollo/springbootsecurityoauth2authorizationserver/SpringBootSecurityOauth2AuthorizationServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSecurityOauth2AuthorizationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityOauth2AuthorizationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityOauth2AuthorizationServerApplication.class, args);
+    }
 
 }
